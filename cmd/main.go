d3bba769c14b681ae8692f305c5c0cc00cbde892
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2023-05-17 21:51:44 +0530

    restructure app

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/extractor"
+	"github.com/unidoc/unipdf/v3/model"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 
 	"invoiceGenerator"
 	"invoiceGenerator/chrome"
@@ -72,11 +83,6 @@ func main() {
 
 func Action(c *cli.Context) error {
 	configFilePath := c.String("config-file")
-	outFilePath := c.String("output-file")
-
-	if outFilePath == "" {
-		outFilePath = filepath.Dir(configFilePath)
-	}
 
 	configFile, err := os.Open(configFilePath)
 	if err != nil {
@@ -103,17 +109,140 @@ func Action(c *cli.Context) error {
 		return err
 	}
 
-	tpl, err := template.Get("invoice.html.tpl")
+	outFilePath := c.String("output-file")
+	if outFilePath == "" {
+		outFilePath = GetFileName(invoice)
+	}
+
+	return GeneratePDF(c.Context, "invoice.html.tpl", outFilePath, invoice)
+}
+
+func GetFileName(invoice *invoiceGenerator.Invoice) string {
+	extension := ".pdf"
+
+	return fmt.Sprintf("%s - %s %d%s", invoice.Number, invoice.Start.Month().String(), invoice.Start.Year(), extension)
+}
+
+func init() {
+	err := license.SetMeteredKey(os.Getenv("UNIDOC_LICENSE_KEY"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+type SummarySheet struct {
+	Month string        `json:"month"`
+	Lines []SummaryLine `json:"lines"`
+	Total float64
+}
+
+type SummaryLine struct {
+	Resource      string
+	InvoiceNumber string
+	Amount        float64
+}
+
+func SummarySheetAction(c *cli.Context) error {
+	invoices := c.StringSlice("invoice")
+
+	sheet := SummarySheet{
+		Month: time.Now().In(time.Local).AddDate(0, -1, 0).Format("January 2006"),
+	}
+
+	sheet.Lines = make([]SummaryLine, len(invoices))
+
+	for i, invoice := range invoices {
+		file, err := os.Open(invoice)
+		if err != nil {
+			return err
+		}
+
+		reader, err := model.NewPdfReader(file)
+		if err != nil {
+			return err
+		}
+
+		page, err := reader.GetPage(1)
+		if err != nil {
+			return err
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return err
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return err
+		}
+
+		splitText := make([]string, 0)
+		for _, tt := range strings.Split(text, "\n") {
+			if strings.TrimSpace(tt) != "" {
+				splitText = append(splitText, tt)
+			}
+		}
+
+		sheet.Lines[i] = SummaryLine{
+			Resource:      GetResource(splitText),
+			InvoiceNumber: GetInvoiceNumber(text),
+			Amount:        GetAmount(splitText),
+		}
+	}
+
+	for _, line := range sheet.Lines {
+		sheet.Total += line.Amount
+	}
+
+	sheet.Total = math.Round(sheet.Total*100) / 100
+
+	outFileName := "summary_sheet_" + strings.ReplaceAll(strings.ToLower(sheet.Month), " ", "_") + ".pdf"
+
+	return GeneratePDF(c.Context, "summary-sheet.html.tpl", outFileName, sheet)
+}
+
+var invoiceNumberRegex = regexp.MustCompile(`INVOICE # (.*)`)
+
+func GetInvoiceNumber(text string) string {
+	matches := invoiceNumberRegex.FindStringSubmatch(text)
+	if len(matches) == 2 {
+		return matches[1]
+	}
+
+	return ""
+}
+
+func GetAmount(splitText []string) float64 {
+	amount, _ := strconv.ParseFloat(strings.Split(splitText[len(splitText)-1], " ")[1], 64)
+
+	return amount
+}
+
+var titleCase = cases.Title(language.English).String
+
+func GetResource(splitText []string) string {
+	return titleCase(splitText[2])
+}
+
+func GeneratePDF(ctx context.Context, templateName, outFileName string, templateData any) error {
+	// get current working directory
+	wd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
 
-	f, err := os.Create(filepath.Join(outFilePath, GetFileName(invoice)+".html"))
+	f, err := os.Create(filepath.Join(wd, outFileName+".html"))
 	if err != nil {
 		return err
 	}
 
-	err = tpl.Execute(f, invoice)
+	tpl, err := template.Get(templateName)
+	if err != nil {
+		return err
+	}
+
+	err = tpl.Execute(f, templateData)
 	if err != nil {
 		return err
 	}
@@ -127,9 +256,9 @@ func Action(c *cli.Context) error {
 
 	err = exec.
 		CommandContext(
-			c.Context, path, "--headless", "--disable-gpu", "--no-pdf-header-footer",
-			"--print-to-pdf="+filepath.Join(outFilePath, GetFileName(invoice)),
-			filepath.Join(outFilePath, GetFileName(invoice)+".html"),
+			ctx, path, "--headless", "--disable-gpu", "--no-pdf-header-footer",
+			"--print-to-pdf="+outFileName,
+			outFileName+".html",
 		).Run()
 	if err != nil {
 		return err
@@ -141,10 +270,4 @@ func Action(c *cli.Context) error {
 	}
 
 	return nil
-}
-
-func GetFileName(invoice *invoiceGenerator.Invoice) string {
-	extension := ".pdf"
-
-	return fmt.Sprintf("%s - %s %d%s", invoice.Number, invoice.Start.Month().String(), invoice.Start.Year(), extension)
 }
